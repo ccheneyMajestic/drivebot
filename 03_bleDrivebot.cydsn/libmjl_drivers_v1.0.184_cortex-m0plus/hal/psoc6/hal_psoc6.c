6fe6895c1b3f89b11412dc623bea09908ff4a157
Craig Cheney <ccheney@majesticlabs.com>
2025-03-10 15:25:17 -0400

    Test the initial mcuPsoc6

---
+++ 03_bleDrivebot.cydsn/libmjl_drivers_v1.0.184_cortex-m0plus/hal/psoc6/hal_psoc6.c
@@ -0,0 +1,304 @@
+/***************************************************************************
+*                                Majestic Labs © 2023
+* File: hal_psoc6.h
+* Workspace: MJL Hardware Abstraction Layer (HAL) Library 
+* Version: v1.0.0
+* Author: C. Cheney
+* Target: PSoC6
+*
+* Brief: Contains wrappers for the MJL library from the Cypress PSoC6 libraries
+*
+* 2023.12.31  - Document Created
+********************************************************************************/
+#include "hal_psoc6.h"
+#include "mjl_errors.h"
+#include "project.h"    /* Cypress files*/
+
+/*******************************************************************************
+* Function Name: uart_psoc6SCB_start()
+********************************************************************************
+* \brief
+*   Start wrapper for a SCB based UART on PSoC6
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t uart_psoc6SCB_start(MLJ_UART_T *const state){   
+    uint32_t error = 0;   
+    (void) state;
+    uartUsb_Start();
+    return error;
+}
+
+/*******************************************************************************
+* Function Name: uart_psoc6SCB_stop()
+********************************************************************************
+* \brief
+*   Stop wrapper for a SCB based UART on PSoC6
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t uart_psoc6SCB_stop(MLJ_UART_T *const state){   
+  uint32_t error = 0;   
+  (void) state;
+  Cy_SCB_UART_Disable(uartUsb_HW, &uartUsb_context);
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: uart_psoc6SCB_writeArrayBlocking()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based UART on PSoC6
+*   Write an array of data via UART using the SCB api. This is a blocking function
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t uart_psoc6SCB_writeArrayBlocking(const uint8_t *array, uint16_t len){
+  uint32_t error = 0;   
+  Cy_SCB_UART_PutArrayBlocking(uartUsb_HW, (void *) array, len);
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: uart_psoc6SCB_read()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based UART on PSoC6
+*   Read a single element from the RX buffer
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t uart_psoc6SCB_read(uint8_t *data){
+  uint32_t error = 0;   
+  if(0 == Cy_SCB_UART_GetNumInRxFifo(uartUsb_HW)){error|= ERROR_UNAVAILABLE;}
+  else {*data = Cy_SCB_UART_Get(uartUsb_HW);}
+  return error;
+}
+
+
+
+/*******************************************************************************
+* Function Name: spi_psoc6SCB_start()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based SPI on PSoC6
+*   Start the SCB Block
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t spi_psoc6SCB_start(MJL_SPI_T *const state){
+  uint32_t error = 0;
+  (void) state;
+  #ifdef USE_SPI
+    SPI_Start();
+  #endif /* USE_SPI */
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: spi_psoc6SCB_stop()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based SPI on PSoC6
+*   Stop the SCB blcok
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t spi_psoc6SCB_stop(MJL_SPI_T *const state){
+  (void) state;
+  uint32_t error = 0;
+  #ifdef USE_SPI
+    Cy_SCB_SPI_Disable(SPI_HW, &SPI_context);
+  #endif /* USE_SPI */
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: spi_psoc6SCB_writeArray_blocking()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based SPI on PSoC6
+*   Write an array of elements
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t spi_psoc6SCB_writeArray_blocking(const uint8_t *array, uint16_t len){
+  uint32_t error = 0;
+  /* Write out the data */
+  #ifdef USE_SPI
+    Cy_SCB_SPI_WriteArrayBlocking(SPI_HW, (void *) array, len);
+  #else 
+    (void) array;
+    (void) len;
+  #endif /* USE_SPI */
+
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: spi_psoc6SCB_read()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based SPI on PSoC6
+*   Place one element from the RX FIFO into the results buffer 
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t spi_psoc6SCB_read(uint8_t *result) {
+  uint32_t error = 0;
+  #ifdef USE_SPI
+      /* Move data from RX FIFO to results */
+      *result = Cy_SCB_SPI_Read(SPI_HW);
+  #else 
+    (void) result;
+  #endif /* USE_SPI */
+
+  
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: spi_psoc6SCB_setActive()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based SPI on PSoC6
+*   Sets an active slave 
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t spi_psoc6SCB_setActive(uint8_t id){
+  uint32_t error = 0;
+  #ifdef USE_SPI
+  /* Write out the data */
+  Cy_SCB_SPI_SetActiveSlaveSelect(SPI_HW, id);
+  #else 
+    (void) id;
+  #endif /* USE_SPI */
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: spi_psoc6SCB_getRxBufferNum()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based SPI on PSoC6
+*   Return the number of elements in the receive buffer
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t spi_psoc6SCB_getRxBufferNum(void){
+    uint32_t num = 0;
+  #ifdef USE_SPI
+    num = Cy_SCB_SPI_GetNumInRxFifo(SPI_HW);
+  #endif /* USE_SPI */
+  return num;
+}
+
+/*******************************************************************************
+* Function Name: spi_psoc6SCB_getTxBufferNum()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based SPI on PSoC6
+*   Return the number of elements in the transmit buffer
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t spi_psoc6SCB_getTxBufferNum(void){
+  uint32_t num = 0;
+  #ifdef USE_SPI
+    num = Cy_SCB_SPI_GetNumInTxFifo(SPI_HW);
+  #endif /* USE_SPI */
+  return num;
+}
+
+/*******************************************************************************
+* Function Name: spi_psoc6SCB_clearRxBuffer()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based SPI on PSoC6
+*   Clear all of the elements in the receive buffer 
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t spi_psoc6SCB_clearRxBuffer(void) {
+  uint32_t error = 0;
+  #ifdef USE_SPI
+  Cy_SCB_SPI_ClearRxFifo(SPI_HW);
+  #endif /* USE_SPI */
+
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: spi_psoc6SCB_clearTxBuffer()
+********************************************************************************
+* \brief
+*   Wrapper for an SCB Based SPI on PSoC6
+*   Clear all of the elements in the transmit buffer 
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t spi_psoc6SCB_clearTxBuffer(void) {
+  uint32_t error = 0;
+  #ifdef USE_SPI
+  Cy_SCB_SPI_ClearTxFifo(SPI_HW);
+  #endif /* USE_SPI */
+
+  return error;
+}
+
+///*******************************************************************************
+//* Function Name: hal_drv_pin_sleep_write()
+//********************************************************************************
+//* \brief
+//*   Control the nSLEEP pin
+//*
+//* \return
+//*  Error code of the operation
+//*******************************************************************************/
+//void hal_drv_pin_sleep_write(uint32_t val) {
+//  #ifdef USE_SPI
+//  Cy_GPIO_Write(pin_DRV_nSLEEP_0_PORT, pin_DRV_nSLEEP_0_NUM, val);
+//  #endif /* USE_SPI */
+//
+//}
+//
+///*******************************************************************************
+//* Function Name: hal_drv_pin_drvoff_write()
+//********************************************************************************
+//* \brief
+//*   Control the DRVOFF pin
+//*
+//* \return
+//*  Error code of the operation
+//*******************************************************************************/
+//void hal_drv_pin_drvoff_write(uint32_t val) {
+//  Cy_GPIO_Write(pin_DRV_DRVOFF_0_PORT, pin_DRV_DRVOFF_0_NUM, val);
+//}
+//
+///*******************************************************************************
+//* Function Name: hal_drv_pin_nfault_read()
+//********************************************************************************
+//* \brief
+//*   Read the nFault pin
+//*
+//* \return
+//*  Error code of the operation
+//*******************************************************************************/
+//uint32_t hal_drv_pin_nfault_read(void){
+//  return Cy_GPIO_Read(pin_DRV_nFAULT_PORT, pin_DRV_nFAULT_NUM); 
+//}
+
---
+++ 03_bleDrivebot.cydsn/main_cm0p.c
@@ -0,0 +1,531 @@
+/***************************************************************************
+*                           Majestic Labs  © 2023
+* File: template_main_psoc6.c
+* Workspace: MJL Driver Templates
+* Version: v1.0
+* Author: Craig Cheney
+*
+* PCB: 
+*
+* Brief:
+*   Template for the PSoC6 Main for the CM0+ Core
+*
+* Change Log:
+*   2023.12.31 - Document created
+********************************************************************************/
+#include "project.h" /* Cypress project */
+#include "mjl_errors.h"
+#include "mjl_uart.h"
+#include "hal_psoc6.h"
+#include "LTC6915.h"
+#include "DRV8244.h"
+#include "max31856.h"
+
+
+/* ####################### BEGIN PROGRAM CONFIGURATION ###################### */
+
+#define MJL_DEBUG
+/* ---------------- DEBUG CASE ----------------
+* Uncomment ONE of the following
+* Debugging will only occur when MJL_DEBUG is defined
+*/
+#ifdef MJL_DEBUG
+//  #define MJL_DEBUG_LED            /* Test the battery charger LED */
+//  #define MJL_DEBUG_UART            /* Test the UART Connection */
+//  #define MJL_DEBUG_BTN /* Test the button */
+  #define MJL_DEBUG_BLE   /* Start ble */
+//  #define MJL_DEBUG_PWM /*  Test PWM  */
+//  #define MJL_DEBUG_TIMERS /* Test the timers */
+//  #define MJL_DEBUG_ADC               /* Read the ADC */
+//  #define MJL_DEBUG_ADC_INT          /* Read the ADC using the interrupt flag */
+//  #define MJL_DEBUG_ADC_AVG          /* Read the ADC and average in software */
+//  #define MJL_DEBUG_SYSTEM_COUNTER      /* Test using the microsecond counter */
+#endif
+/* -------------- END DEBUG CASE --------------  */
+
+/* Enable SPI */
+
+/* ############################# BEGIN PROGRAM ############################## */
+/* Macro Definitions */
+#define PWM_DRV_PERIOD (999)
+#define PWM_DRV_DUTY_50  (PWM_DRV_PERIOD+1)/2
+#define PWM_DRV_DUTY_100  (PWM_DRV_PERIOD+1)
+#define ADC_CHAN_DRV_IPROP (0) /* Current measure of the DRV */
+#define ADC_SAMPLES_PER_SEC  (939) /* Rate of the ADC in [Hz] */
+#define TEMP_SENSE_ID  (0)
+#define DRV_R_IPROPI   (1000.0) /* Shunt resistor value in [Ω] */
+#define DRV8244_GAIN_IPROPI_QFN       (4750.0) /* Current gain in [A/A] */
+#define DRV_VIPROPI_TO_I  (DRV8244_GAIN_IPROPI_QFN / DRV_R_IPROPI) /* [1/Ω] - Multiply V_ipropi by this to get the current in [A]  */
+
+/* System timer */
+typedef struct {
+  uint32_t second;
+  uint32_t microsecond;
+} system_time_s;
+
+uint32_t get_system_time_atomic(system_time_s *const state);
+
+uint32_t drv8244_current_from_voltage(drv8244_state_s *const, float32_t voltage); 
+
+void set_rgb(uint8_t red, uint8_t green, uint8_t blue);
+
+/* Global Variables */
+volatile bool flag_timer_second;
+volatile bool flag_adc;
+MLJ_UART_S usb;
+MJL_SPI_S spi;
+max31856_state_s tempSense;
+drv8244_state_s drv;
+system_time_s time;
+
+/* ISR Declarations */
+void isr_adc(void);
+void isr_second(void);
+
+/* Function Definitions */
+uint32_t initHardware(void);
+void breatheLed_setState(bool state);
+
+/* Main Program */
+#ifndef MJL_DEBUG
+/*******************************************************************************
+* Function Name: main()
+********************************************************************************
+* Summary:
+*   The top-level application function for the project.
+*
+*******************************************************************************/
+int main(void) {
+
+  initHardware();
+  uart_printHeader(&usb, "MJL_TEMPLATE_MAIN", __DATE__, __TIME__);
+  CyDelay(10);
+  uart_println(&usb, "");
+  uart_println(&usb,"* Press 'Enter' to reset");
+  /* Enable CM4.  CY_CORTEX_M4_APPL_ADDR must be updated if CM4 memory layout is changed. */
+  //  Cy_SysEnableCM4(CY_CORTEX_M4_APPL_ADDR); 
+  
+  for(;;){
+    /* Handle the UART */
+    uint8_t readVal = 0;
+    uint32_t readError = uart_read(&usb, &readVal);
+    if(!readError) {
+      /* Reset on Enter */
+      if('\r' == readVal) {
+        uart_print(&usb, "\r\nResetting...");
+        CyDelay(1000);
+        /* Reset both cores */
+        NVIC_SystemReset();
+      }
+    }
+  }    
+}
+#endif /* ifndef MJL_DEBUG*/
+/* End Main Program */
+
+
+/* ############################ BEGIN DEBUGGER ############################## */
+#ifdef MJL_DEBUG
+/*******************************************************************************
+* DEBUGGER: main()
+********************************************************************************
+* Summary:
+*   Debugging function for the PCB hardware
+*******************************************************************************/
+int main(void){
+  #warning "MJL_DEBUG is enabled"
+  initHardware();
+  
+  /* Test Cases */
+  #ifdef MJL_DEBUG_LED
+    /* Cycle the RGB LED */    
+    uint8_t brightness = 25;
+    for(;;) {
+        set_rgb(brightness, 0, 0);
+        CyDelay(500);
+        set_rgb(0, brightness, 0);
+        CyDelay(500);
+        set_rgb(0, 0, brightness);
+        CyDelay(500);   
+        /* Increase the brightness */
+        brightness += 25;
+        if(brightness > 100){
+            brightness = 25;
+        }
+    }
+  /* End MJL_DEBUG_LED */
+  #elif defined MJL_DEBUG_UART             
+  /* Test the UART Connection */
+  uart_printHeader(&usb, "MJL_DEBUG_UART", __DATE__, __TIME__);
+  CyDelay(10);
+  uart_println(&usb, "");
+  uart_println(&usb,"* Press 'Enter' to reset");
+  uart_println(&usb, "");
+  set_rgb(0, 0, 50);
+
+  for(;;) {
+    uint8_t readVal = 0;
+    uint32_t readError = uart_read(&usb, &readVal);
+    if(!readError) {
+      /* Echo UART */
+      uart_write(&usb, readVal);                
+      /* Reset on Enter */
+      if('\r' == readVal) {
+        uart_print(&usb, "\r\nResetting...");
+        CyDelay(1000);
+        /* Reset both cores */
+        NVIC_SystemReset();
+      }
+    }
+  }
+  #elif defined MJL_DEBUG_BTN
+    /* Test the button */
+    uart_printHeader(&usb, "MJL_DEBUG_BTN", __DATE__, __TIME__);
+    CyDelay(10);
+    uart_println(&usb, "");
+    uart_println(&usb,"* Press 'Enter' to reset");
+    uart_println(&usb, "");
+    set_rgb(0, 0, 50);
+
+    /* Read the button state */
+    bool stateBtn = false;
+    bool stateBtn_prev = true;
+    
+    for(;;) {
+      uint8_t readVal = 0;
+      uint32_t readError = uart_read(&usb, &readVal);
+      if(!readError) {
+        /* Echo UART */
+        uart_write(&usb, readVal);                
+        /* Reset on Enter */
+        if('\r' == readVal) {
+          uart_print(&usb, "\r\nResetting...");
+          CyDelay(1000);
+          /* Reset both cores */
+          NVIC_SystemReset();
+        }
+      }
+
+      stateBtn = Cy_GPIO_Read(pin_BTN_0_PORT, pin_BTN_0_NUM);
+      if(stateBtn != stateBtn_prev){
+        stateBtn_prev = stateBtn;
+        uart_printlnf(&usb, "Btn: %b", stateBtn);
+        Cy_GPIO_Write(pin_LED_EXT_0_PORT, pin_LED_EXT_0_NUM, stateBtn);
+      }
+    }
+    
+  #elif defined MJL_DEBUG_BLE   
+    /* Start ble */
+    uart_printHeader(&usb, "MJL_DEBUG_BLE", __DATE__, __TIME__);
+    CyDelay(10);
+    uart_println(&usb, "");
+    uart_println(&usb,"* Press 'Enter' to reset");
+    uart_println(&usb, "");
+    set_rgb(0, 0, 50);
+
+    /* Read the button state */
+    bool stateBtn = false;
+    bool stateBtn_prev = true;
+    
+    for(;;) {
+      uint8_t readVal = 0;
+      uint32_t readError = uart_read(&usb, &readVal);
+      if(!readError) {
+        /* Echo UART */
+        uart_write(&usb, readVal);                
+        /* Reset on Enter */
+        if('\r' == readVal) {
+          uart_print(&usb, "\r\nResetting...");
+          CyDelay(1000);
+          /* Reset both cores */
+          NVIC_SystemReset();
+        }
+      }
+
+      stateBtn = Cy_GPIO_Read(pin_BTN_0_PORT, pin_BTN_0_NUM);
+      if(stateBtn != stateBtn_prev){
+        stateBtn_prev = stateBtn;
+        uart_printlnf(&usb, "Btn: %b", stateBtn);
+        Cy_GPIO_Write(pin_LED_EXT_0_PORT, pin_LED_EXT_0_NUM, stateBtn);
+        if(stateBtn){
+          /* Green when button pressed */
+          set_rgb(0, 50, 0);
+        } else {
+          /* Blue when button not pressed */
+          set_rgb(0, 0, 50);
+        }
+      }
+    }
+    
+
+  #elif defined MJL_DEBUG_PWM           
+    /* Basic usage the DRV driver chip */
+    uart_printHeader(&usb, "MJL_DEBUG_DRV", __DATE__, __TIME__);
+    CyDelay(10);
+    uart_println(&usb, "");
+    uart_println(&usb, "* Press ' ' to toggle PWM");
+    uart_println(&usb, "");
+          
+    bool pwmState = false;
+    bool prevPwmState = true;
+        
+    for(;;) {
+      /* Check for UART input */
+      uint8_t readVal = 0;
+      uint32_t readError = uart_read(&usb, &readVal);
+      if(readVal == ' ') {
+          pwmState = !pwmState;
+      }
+      /* Detect State Change */
+      if(pwmState != prevPwmState) {
+          prevPwmState = pwmState;
+          uart_println(&usb, CMD_CLEAR_LINEUP);
+          if(pwmState) {
+              PWM_DRV_Enable();
+              Cy_TCPWM_TriggerStart(PWM_DRV_HW, PWM_DRV_CNT_MASK);
+              breatheLed_setState(true);
+          }
+          else {
+              PWM_DRV_Disable();
+              breatheLed_setState(false);
+          }
+          uart_printlnf(&usb, "PWM Enabled: %b", pwmState);
+        }
+    }
+  /* End MJL_DEBUG_PWM */
+  #elif defined MJL_DEBUG_TIMERS 
+    /* Test the timers */
+        /* Basic usage the DRV driver chip */
+    uart_printHeader(&usb, "MJL_DEBUG_DRV", __DATE__, __TIME__);
+    CyDelay(10);
+    uart_println(&usb, "");
+    uart_println(&usb, "");
+         
+    for(;;) {
+      /* Handle Print timer */
+      if(flag_timer_second) {
+        flag_timer_second = false;
+        uart_println(&usb, "a");
+      }
+      /* Handle temperature error */
+      uint8_t readVal = 0;
+      uint32_t readError = uart_read(&usb, &readVal);
+      if(!readError) {  
+        /* Reset on Enter */
+        if('\r' == readVal) {
+          uart_print(&usb, "\r\nResetting...");
+          CyDelay(1000);
+          /* Reset both cores */
+          NVIC_SystemReset();
+        }
+      }
+    }
+  #elif defined MJL_DEBUG_ADC          
+    /* Start and read the ADC */
+    uart_printHeader(&usb, "MJL_DEBUG_ADC", __DATE__, __TIME__);
+    CyDelay(10);
+    uart_println(&usb, "\r\n");
+    
+    ADC_Start();
+    ADC_StartConvert();
+    
+    for(;;) {
+        ADC_IsEndConversion(CY_SAR_WAIT_FOR_RESULT);
+        uint16_t val = ADC_GetResult16(ADC_CHAN_DRV_IPROP);
+        uart_printlnf(&usb, "ADC: 0x%x (%d)", val, val);
+        CyDelay(1000);
+    }
+        
+    /* End MJL_DEBUG_ADC */
+    #elif defined MJL_DEBUG_ADC_INT          
+      /* Read the ADC using the interrupt flag */
+      uart_printHeader(&usb, "MJL_DEBUG_ADC_INT", __DATE__, __TIME__);
+      CyDelay(10);
+      uart_println(&usb, "\r\n");
+      
+      /* Start the ADC Conversions */
+      Cy_SAR_StartConvert(ADC_SAR__HW, CY_SAR_START_CONVERT_CONTINUOUS);
+      
+      uint16_t adcVal = 0;
+      for(;;) {
+          /* ADC is available for servicing */
+          if(flag_adc) {
+              flag_adc = false;
+              adcVal = ADC_GetResult16(ADC_CHAN_DRV_IPROP);
+          }
+      }
+    /* End MJL_DEBUG_ADC */ 
+  #elif defined MJL_DEBUG_ADC_AVG          
+    /* Read the ADC and average */
+    uart_printHeader(&usb, "MJL_DEBUG_ADC_AVG", __DATE__, __TIME__);
+    CyDelay(10);
+    uart_println(&usb, "\r\n");
+      
+    uint32_t ongoingSum = 0;
+    uint16_t iteration = 0;
+    const uint16_t samplesPerSecond = 939; 
+    
+    /* Start the ADC Conversions */
+    Cy_SAR_StartConvert(ADC_SAR__HW, CY_SAR_START_CONVERT_CONTINUOUS);
+
+    for(;;) {
+      /* ADC is available for servicing */
+      if(flag_adc) {
+        flag_adc = false;
+        uint16_t adcVal = ADC_GetResult16(ADC_CHAN_DRV_IPROP);
+        ongoingSum += adcVal;
+        iteration++;
+        if(samplesPerSecond <=  iteration) {
+          Cy_GPIO_Write(pin_DEBUG_ADC_SUM_PORT, pin_DEBUG_ADC_SUM_NUM, 1);
+          uint32_t mean = ongoingSum/iteration;
+          ongoingSum = 0;
+          iteration = 0;
+          uart_printlnf(&usb, "%d", mean);
+          Cy_GPIO_Write(pin_DEBUG_ADC_SUM_PORT, pin_DEBUG_ADC_SUM_NUM, 0);
+        }
+      }
+    } 
+    /* End MJL_DEBUG_ADC_AVG */ 
+  #elif defined MJL_DEBUG_SYSTEM_COUNTER      
+    /* Test using the minute counter */
+    uart_printHeader(&usb, "MJL_DEBUG_SYSTEM_COUNTER", __DATE__, __TIME__);
+    CyDelay(10);
+    uart_println(&usb, "");
+    uart_println(&usb,"* Press 'Enter' to reset");
+    uart_println(&usb, "");
+    
+    for(;;) {
+      /* Triggered every second */
+      if(flag_timer_second){
+        flag_timer_second = false;
+        get_system_time_atomic(&time);
+        uart_printlnf(&usb, "sec:%d, micro:%d", time.second, time.microsecond);
+      }
+      /* Handle the UART */
+      uint8_t readVal = 0;
+      uint32_t readError = uart_read(&usb, &readVal);
+      if(!readError) {
+        /* Echo UART */
+        //uart_write(&usb, readVal);                
+        /* Reset on Enter */
+        if('\r' == readVal) {
+          uart_print(&usb, "\r\nResetting...");
+          CyDelay(1000);
+          /* Reset both cores */
+          NVIC_SystemReset();
+        }
+      }
+    }
+  #endif 
+  /* Fall through Infinite loop */
+  for(;;){}
+}
+#endif /* MJL_DEBUG */
+
+
+/* ####################### BEGIN FUNCTION DEFINITIONS ####################### */
+
+/*******************************************************************************
+* Function Name: initHardware()
+********************************************************************************
+* \brief
+*   Configures the hardware to start
+*
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t initHardware(void) {
+  uint32_t error = 0;
+    
+  /* Start the RGB led */
+  pwm_led_R_Start();
+  pwm_led_G_Start();
+  pwm_led_B_Start();
+
+  /* Start the UART */
+  MJL_UART_CFG_S uartCfg = uart_cfg_default;
+  uartCfg.hal_req_writeArray = uart_psoc6SCB_writeArrayBlocking;
+  uartCfg.hal_req_read = uart_psoc6SCB_read;
+  uartCfg.hal_opt_externalStart = uart_psoc6SCB_start;
+  uartCfg.hal_opt_externalStop = uart_psoc6SCB_stop;
+  error |= uart_init(&usb, &uartCfg);
+  error |= uart_start(&usb);
+   
+//  /*Connect the microsecond timer to the second interrupt */
+//  Cy_SysInt_Init(&interrupt_second_cfg, isr_second);
+//  NVIC_ClearPendingIRQ(interrupt_second_cfg.intrSrc);
+//  NVIC_EnableIRQ(interrupt_second_cfg.intrSrc);
+//  counter_microsecond_Start();
+//  counter_second_Start();
+//  
+//  /* Enable the ADC  */
+//  Cy_SysInt_Init(&interrupt_adc_cfg, isr_adc);
+//  NVIC_ClearPendingIRQ(interrupt_adc_cfg.intrSrc);
+//  NVIC_EnableIRQ(interrupt_adc_cfg.intrSrc);
+//  ADC_Start();
+//  NVIC_EnableIRQ(ADC_IRQ_cfg.intrSrc);
+  
+  /* Start global interrupts */
+  __enable_irq(); /* Enable global interrupts. */
+  
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: isr_second()
+********************************************************************************
+* \brief
+*   Triggers after 1 second
+*
+*******************************************************************************/
+void isr_second(void){
+  flag_timer_second = true;
+  counter_microsecond_ClearInterrupt(CY_TCPWM_INT_ON_TC);
+  NVIC_ClearPendingIRQ(interrupt_second_cfg.intrSrc);
+}
+
+
+/*******************************************************************************
+* Function Name: isr_adc()
+********************************************************************************
+* \brief
+*   Triggers when the ADC End of Scan is complete (EOS)
+*
+*******************************************************************************/
+void isr_adc(void) {
+  flag_adc = true;
+  NVIC_ClearPendingIRQ(interrupt_adc_cfg.intrSrc);
+}
+
+/*******************************************************************************
+* Function Name: get_system_time_atomic()
+********************************************************************************
+* \brief
+*   Atomically updates the system time state structure passed in 
+*
+*******************************************************************************/
+uint32_t get_system_time_atomic(system_time_s *const state){
+ uint32_t error = 0;
+  /* Get atomically */
+  uint8_t intr_state = Cy_SysLib_EnterCriticalSection();
+  state->microsecond = Cy_TCPWM_Counter_GetCounter(counter_microsecond_HW, counter_microsecond_CNT_NUM);
+  state->second = Cy_TCPWM_Counter_GetCounter(counter_second_HW, counter_second_CNT_NUM);
+  Cy_SysLib_ExitCriticalSection(intr_state);
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: set_rgb()
+********************************************************************************
+* \brief
+*   Set the RGB led input is [0-100] as a duty ration
+*
+*******************************************************************************/
+void set_rgb(uint8_t red, uint8_t green, uint8_t blue){
+    Cy_TCPWM_PWM_SetCompare0(pwm_led_R_HW, pwm_led_R_CNT_NUM, red);
+    Cy_TCPWM_PWM_SetCompare0(pwm_led_G_HW, pwm_led_G_CNT_NUM, green);
+    Cy_TCPWM_PWM_SetCompare0(pwm_led_B_HW, pwm_led_B_CNT_NUM, blue);
+}
+
+
+/* [] END OF FILE */
